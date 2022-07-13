f5ee8083110ebfd920a22eb8c9ec91a867a2d260
Twoot <milanbartky@gmail.com>
2022-07-13 16:03:11 +0200

    Added some more debug info

--- debug.go
+++ debug.go
@@ -3,9 +3,11 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"github.com/mitchellh/go-ps"
 	"github.com/skratchdot/open-golang/open"
 	"io/ioutil"
 	"path/filepath"
+	"strings"
 )
 
 type DebugInfo struct {
@@ -38,6 +40,12 @@ func dumpDebugData() {
 
 	buf.WriteString("\r\n")
 
+	buf.WriteString(FindProcess("SDeck", "StreamDeck.exe"))
+	buf.WriteString(FindProcess("FiveM", "FiveM.exe"))
+	buf.WriteString(FindProcess("GTA-V", "GTAProcess.exe"))
+
+	buf.WriteString("\r\n")
+
 	if info.LastError != nil {
 		buf.WriteString(fmt.Sprintf("LastError: %s", info.LastError.Error()))
 	} else {
@@ -47,4 +55,31 @@ func dumpDebugData() {
 	_ = ioutil.WriteFile(path, buf.Bytes(), 0777)
 
 	_ = open.Start(path)
+}
+
+func FindProcess(label, name string) string {
+	pid, name, err := _findProc(name)
+
+	if err != nil {
+		return fmt.Sprintf("%s PID: %s\r\n", label, err.Error())
+	}
+
+	return fmt.Sprintf("%s PID: %d (%s)\r\n", label, pid, name)
+}
+
+func _findProc(name string) (int, string, error) {
+	proc, err := ps.Processes()
+	if err != nil {
+		return 0, "", err
+	}
+
+	name = strings.ToLower(name)
+
+	for _, p := range proc {
+		if strings.Contains(strings.ToLower(p.Executable()), name) {
+			return p.Pid(), p.Executable(), nil
+		}
+	}
+
+	return 0, "", nil
 }
